fb30c8ecc5ce47e035e803c8cd1923bcee66e0e9
Elaina <userelaina@pm.me>
2024-01-21 20:56:06 +0000

    :white_check_mark:

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,10 @@
+g++ "badapple-ffmpeg.cpp" -o "ba.exe" -w -g -O3 -static
+g++ "badapple-opencv.cpp" -o "ba.exe" -w -g -O3 -Bdynamic -I"$Env:OPENCV481\include" -L"$Env:OPENCV481\x64\mingw\lib" -llibopencv_world481
+
 ./ba --qwq
-./ba -i archive/Elysia1.mp4 -o archive/ba.badapple -r 10
-./ba -i archive/ba.badapple -a archive/badapple.mp4
-./ba -i archive/NCOP.mkv -s 96:54 --contrast_enhancement --play_audio
 ./ba
+./ba -i aaa
+./ba -i archive/Elysia1.mp4 -o archive/ely.badapple -s 128:72 -r 10
+./ba -i archive/ely.badapple -a archive/addiction.flac
+./ba -i archive/NCOP.mkv -s 96:54 --contrast --play_audio
+./ba -i archive/87011701_p0.jpg -s 85:98 --contrast
--- test/test_no_audio.sh
+++ test/test_no_audio.sh
@@ -1,5 +1,10 @@
+g++ "badapple-ffmpeg.cpp" -o "ba" -w -g -O3 -static
+g++ "badapple-opencv.cpp" -o "ba" -w -g -O3 -Bdynamic `pkg-config --cflags --libs opencv4`
+
 ./ba --qwq
-./ba -i badapple.mp4 -o ba.badapple -r 10
-./ba -i ba.badapple
-./ba -i badapple.mp4 -s 96:54 --contrast_enhancement
 ./ba
+./ba -i aaa
+./ba -i archive/Elysia1.mp4 -o archive/ely.badapple -s 128:72 -r 10
+./ba -i archive/ely.badapple
+./ba -i archive/NCOP.mkv -s 96:54 --contrast
+./ba -i archive/87011701_p0.jpg -s 85:98 --contrast
