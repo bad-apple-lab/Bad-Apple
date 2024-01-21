7bdf3e106843e450e27e09bb01918491c767bb66
Elaina <userelaina@pm.me>
2024-01-21 22:51:44 +0000

    :white_check_mark: auto scale

--- test/test.sh
+++ test/test.sh
@@ -4,7 +4,7 @@ g++ "badapple-opencv.cpp" -o "ba.exe" -w -g -O3 -Bdynamic -I"$Env:OPENCV481\incl
 ./ba --qwq
 ./ba
 ./ba -i aaa
-./ba -i archive/Elysia1.mp4 -o archive/ely.badapple -s 128:72 -r 10
+./ba -i archive/Elysia1.mp4 -o archive/ely.badapple -s 0:72 -r 10
 ./ba -i archive/ely.badapple -a archive/addiction.flac
-./ba -i archive/NCOP.mkv -s 96:54 --contrast --play_audio
+./ba -i archive/NCOP.mkv -s 96:0 --contrast --play_audio
 ./ba -i archive/87011701_p0.jpg -s 85:98 --contrast
--- test/test_no_audio.sh
+++ test/test_no_audio.sh
@@ -4,7 +4,7 @@ g++ "badapple-opencv.cpp" -o "ba" -w -g -O3 -Bdynamic `pkg-config --cflags --lib
 ./ba --qwq
 ./ba
 ./ba -i aaa
-./ba -i archive/Elysia1.mp4 -o archive/ely.badapple -s 128:72 -r 10
+./ba -i archive/Elysia1.mp4 -o archive/ely.badapple -s 0:72 -r 10
 ./ba -i archive/ely.badapple
-./ba -i archive/NCOP.mkv -s 96:54 --contrast
+./ba -i archive/NCOP.mkv -s 96:0 --contrast
 ./ba -i archive/87011701_p0.jpg -s 85:98 --contrast
