35964547e8173c33b8c04a114c769bba540b0557
Elaina <userelaina@pm.me>
2024-01-21 18:09:03 +0000

    :white_check_mark:

--- test/test.ps1
+++ test/test.ps1
@@ -1,11 +1,6 @@
-./badapple-opencv-v2.1.0-win-amd64.exe --qwq
-./badapple-opencv-v2.1.0-win-amd64.exe -i Elysia1.mp4 -o ba.badapple -r 10
-./badapple-opencv-v2.1.0-win-amd64.exe -i ba.badapple -a badapple.mp4
-./badapple-opencv-v2.1.0-win-amd64.exe -i NCOP.mkv -s 96:54 --contrast_enhancement --play_audio
-./badapple-opencv-v2.1.0-win-amd64.exe
-
-./badapple-ffmpeg-v2.1.0-win-amd64.exe --qwq
-./badapple-ffmpeg-v2.1.0-win-amd64.exe -i Elysia1.mp4 -o ba.badapple -r 10
-./badapple-ffmpeg-v2.1.0-win-amd64.exe -i ba.badapple -a badapple.mp4
-./badapple-ffmpeg-v2.1.0-win-amd64.exe -i NCOP.mkv -s 96:54 --contrast_enhancement --play_audio
-./badapple-ffmpeg-v2.1.0-win-amd64.exe
+./ba --qwq
+./ba --play_audio
+./ba -i archive/Elysia1.mp4 -o archive/ba.badapple -r 10
+./ba -i archive/ba.badapple -a archive/badapple.mp4
+./ba -i archive/NCOP.mkv -s 96:54 --contrast_enhancement --play_audio
+./ba
