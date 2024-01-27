7b378235341b9160b1c990e5da0cae82365ba06a
Elaina <userelaina@pm.me>
2024-01-27 08:07:31 +0000

    :white_check_mark: add tests for player

--- test/test.sh
+++ test/test.sh
@@ -6,5 +6,6 @@ g++ "badapple-opencv.cpp" -o "ba.exe" -w -g -O3 -Bdynamic -I"$Env:OPENCV481\incl
 ./ba -i aaa
 ./ba -i archive/Elysia1.mp4 -o archive/ely.badapple -s 0:72 -r 10
 ./ba -i archive/ely.badapple -a archive/addiction.flac
-./ba -i archive/NCOP.mkv -s 96:0 --contrast --play_audio
+./ba -i archive/NCOP.mkv -s 96:0 --contrast -p mpv
 ./ba -i archive/87011701_p0.jpg -s 85:98 --contrast
+./ba -a archive/badapple.mp3 -p mpg123
